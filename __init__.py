36b6abb0fc07bf40e862aa78cc22a36e47b394fa
Bruno Humic <humic.bruno@gmail.com>
2018-09-25 17:08:37 +0200

    initial working version

--- __init__.py
+++ __init__.py
@@ -1,7 +1,30 @@
 from binaryninja import *
+import re
 
-def do_nothing(bv,function):
-	show_message_box("Do Nothing", "Congratulations! You have successfully done nothing.\n\n" +
-					 "Pat yourself on the back.", MessageBoxButtonSet.OKButtonSet, MessageBoxIcon.ErrorIcon)
+def import_map(bv,function):
+    
+    map_path = get_open_filename_input("MAP file...")
+    
+    if not map_path:
+        log_error("No MAP file selected")
+        return
 
-PluginCommand.register_for_address("Useless Plugin", "Basically does nothing", do_nothing)
+    with open(map_path, "r") as f_map:
+        # First line is empty
+        if f_map.readline().strip():
+            log_error("Invalid MAP file format")
+            return
+        
+        # Second line is field values
+        if not re.match(r"^Start\s+Length\s+Name\s+Class$", f_map.readline().strip()):
+            log_error("Invalid MAP file format")
+            return
+
+        # Read lines until the end and create symbols for valid functions
+        for current_line in f_map:
+            m = re.match(r"^(?P<address>[a-fA-F0-9]{5,16})\s+(function|procedure)\s+(?P<library>[\w]+)\.(?P<procedure>[\w]+)(:|\(|;)", current_line.strip())
+            if m:
+                bv.define_user_symbol(Symbol(SymbolType.DataSymbol, int(m.group("address"), 16), m.group("procedure")))
+
+
+PluginCommand.register_for_address("Import MAP from IDR...", "Import MAP", import_map)
