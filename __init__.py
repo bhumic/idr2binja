58f262f3e056611d1612d751004587ae5853e95c
Bruno Humic <humic.bruno@gmail.com>
2018-09-28 15:57:09 +0200

    added support for 1 type of expressions. Still need to fix bug with files with two many lines

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
 from binaryninja import *
 import re
 
+def create_tmp(base, depth):
+    """
+    Split the base file into several smaller ones based on depth.
+    For some reason Binary Ninja does not want to process the base
+    file correctly if it is too big.
+    """
+    pass
+
 def import_map(bv,function):
     
     map_path = get_open_filename_input("MAP file...")
@@ -21,10 +29,14 @@ def import_map(bv,function):
             return
 
         # Read lines until the end and create symbols for valid functions
-        for current_line in f_map:
-            m = re.match(r"^(?P<address>[a-fA-F0-9]{5,16})\s+(function|procedure)\s+(?P<library>[\w]+)\.(?P<procedure>[\w]+)(:|\(|;)", current_line.strip())
-            if m:
-                bv.define_user_symbol(Symbol(SymbolType.DataSymbol, int(m.group("address"), 16), m.group("procedure")))
+        for current_line in f_map.readlines():
+            match = re.match(r"^(?P<address>[a-fA-F0-9]{5,16})\s+(function)\s+(?P<library>[\w]+)\.(?P<procedure>[\w]+):(?P<return>[\w]+);\s+[\w]+;$", current_line.strip())
+            if match:
+                address = int(match.group("address"), 16)
+                if not bv.get_function_at(address):
+                    bv.add_function(address)
+                if not bv.get_symbol_at(address):
+                    bv.define_user_symbol(Symbol(SymbolType.FunctionSymbol, address, match.group("procedure")))
 
 
 PluginCommand.register_for_address("Import MAP from IDR...", "Import MAP", import_map)
